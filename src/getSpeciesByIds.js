b0d5db4c6e473a2ab71983640ddab586e2b6930b
Andre Felipe <afelipes@gmail.com>
2022-01-25 18:33:56 -0300

    Requisito 1 - Implemente a função getSpeciesByIds (AJUSTES LINT)

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,4 +1,5 @@
 const data = require('../data/zoo_data');
+
 const matchSpecie = [];
 
 function getSpeciesByIds(...ids) {
@@ -18,7 +19,6 @@ function getSpeciesByIds(...ids) {
     });
     return matchSpecie;
   }
-
 }
 
 // console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
