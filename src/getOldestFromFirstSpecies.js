6dd05e712b19371056cc8ddc60eab971ca2a2b8a
Andre Felipe <afelipes@gmail.com>
2022-01-27 17:43:07 -0300

    Requisito 9 - Implemente a função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecie = employees.find((item) => item.id === id).responsibleFor[0];
+  const arraySpecie = species.find((item) => item.id === firstSpecie)
+    .residents.sort((a, b) => b.age - a.age)[0];
+  // return [arraySpecie.name, arraySpecie.sex, arraySpecie.age];
+  return Object.values(arraySpecie);
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
